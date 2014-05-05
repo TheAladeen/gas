d285782aa3507998d4807c42fec5812f38027b97
featen <jason@featen.com>
2014-05-05 11:36:51 +0000

    refresh google adw

--- webapp/js/controllers/dict.js
+++ webapp/js/controllers/dict.js
@@ -1,11 +1,15 @@
 angular.module('featen.dict').controller('DictController', ['$scope', '$routeParams', '$location', 'Global', 'Dict', function($scope, $routeParams, $location, Global, Dict) {
         $scope.global = Global;
 
+	$scope.init = function() {
+		(adsbygoogle = window.adsbygoogle || []).push({});
+	};
         $scope.query = function() {
 			var q = $scope.searchtext;
     		if ($scope.searchtext.length == 0)
     			return;
            	Dict.query(q, function(r) {
+
                	$scope.fanyi = r;
             });
 
